a31038ee69012a8bf45bb1db97b53d4f2019942a
Dongmyeong Lee <dongmyeong.lee22@gmail.com>
2020-10-13 21:36:01 +0900

    오브젝트 ch3, 4, 5

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # 공부한 것을 간단히 요약 정리 
 
 ### [토비의 스프링](./spring-toby/README.md)
-
+### [오브젝트](./object/README.md)
--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,24 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RestController
+	public static class TempC {
+		@GetMapping("/")
+		public String go(HttpServletRequest httpServletRequest) {
+			return "helloWOrld";
+		}
 	}
 
 }
